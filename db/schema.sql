d0d7a99b437cb20e95cb889928f8e2a72c4da4d0
berekt24 <bashenafi35@gmail.com>
2022-12-01 21:24:02 -0500

    changed schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -3,14 +3,14 @@ CREATE DATABASE business_db;
 
 USE business_db;
 
-CREATE TABLE department(
+CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   roles_id INT,
-  department_name VARCHAR(30) NOT NULL,
+  department_name VARCHAR(30) NOT NULL
   
 );
 
-CREATE TABLE roles(
+CREATE TABLE roles (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL, 
@@ -18,11 +18,12 @@ CREATE TABLE roles(
   FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-CREATE TABLE employees(
+CREATE TABLE employees (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT,
+  roles_id INT,
   manager_id INT, 
   FOREIGN KEY(manager_id) REFERENCES employees(id)
+  FOREIGN KEY(roles_id) REFERENCES roles(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,25 +1,23 @@
 USE business_db;
 
 INSERT INTO department (department_name, roles_id)
-VALUES ("Lion King", 1),
-       ("The Godfather", 2),
-       ("West Side Story", 3),
-       ("Parasite", 4),
-       ("The Wizard of Oz", 5);
+VALUES ("Human Resources", 1),
+       ("Food Services", 2),
+       ("Customer Service", 3),
+       ("Information Technology", 4),
+       ("Accounting", 5);
 
 INSERT INTO roles (title, salary, department_id)
-VALUES ("hello", 100, 1),
-       ("world", 200, 2),
-       ("green", 134, 3),
-       ("blue", 343, 4),
-       ("red", 929, 5);
+VALUES ("Human Resource Manager", 80000, 1),
+       ("Food Service Manager", 100000, 2),
+       ("Customer Service Manager", 90000, 3),
+       ("Information Technology Manager", 200000, 4),
+       ("Accountant", 120000, 5);
 
 INSERT INTO employees(id, first_name, last_name, roles_id, manager_id)
 VALUES
-(1, 'frank', 'stevenson', '1', '1'),
-(2, 'Kobe', 'Bryant', '2', '2'),
-(3, 'john', 'trill', '3', '3'),
+(1, 'jonah', 'hill', '1', '1'),
+(2, 'seth', 'rogen', '2', '2'),
+(3, 'morgan', 'freeman', '3', '3'),
 (4, 'will', 'smith', '4', '4'),
-(5,'craig', 'robinson', '5', '5'),
-(6, 'jay', 'williams','1', '1'),
-(7, 'greg', 'seymour','2', '2');
+(5,'jada', 'pinkett', '5', '5');
