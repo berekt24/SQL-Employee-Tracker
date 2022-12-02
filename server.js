7a72c92c042024199581e2c7d447a6f3c856356a
berekt24 <bashenafi35@gmail.com>
2022-12-01 19:51:30 -0500

    server setup

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-const express = require('express');
-// Import and require mysql2
+const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
@@ -16,8 +15,15 @@ const db = mysql.createConnection(
     // MySQL Username
     user: 'root',
     // TODO: Add MySQL Password
-    password: '',
-    database: 'books_db'
+    password: '19971024',
+    database: 'business_db'
   },
-  console.log(`Connected to the books_db database.`)
+  console.log(`Connected to the business_db database.`)
 );
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
