4d28704847c684b956f8d55a6a8dffb297f14c3f
berekt24 <bashenafi35@gmail.com>
2022-12-01 20:12:56 -0500

    schema and seed testing

--- db/schema.sql
+++ db/schema.sql
@@ -3,14 +3,14 @@ CREATE DATABASE business_db;
 
 USE business_db;
 
-CREATE TABLE department (
+CREATE TABLE department(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   roles_id INT,
   department_name VARCHAR(30) NOT NULL,
   
 );
 
-CREATE TABLE roles (
+CREATE TABLE roles(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL, 
@@ -18,7 +18,7 @@ CREATE TABLE roles (
   FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-CREATE TABLE employees (
+CREATE TABLE employees(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,25 @@
-USE 
+USE business_db;
 
-INSERT INTO department (department_name)
-VALUES ("Lion King"),
-       ("The Godfather"),
-       ("West Side Story"),
-       ("Parasite"),
-       ("The Wizard of Oz");
+INSERT INTO department (department_name, roles_id)
+VALUES ("Lion King", 1),
+       ("The Godfather", 2),
+       ("West Side Story", 3),
+       ("Parasite", 4),
+       ("The Wizard of Oz", 5);
 
 INSERT INTO roles (title, salary, department_id)
-VALUES ("hello",100, 1),
-       ("world",200, 2),
-       ("green",134, 3),
-       ("blue",343, 4),
-       (5, "The tin man gave a metallic, hollow performance"),
-       (1, "Hakuna matata"),
-       (5, "Those flying monkeys are nightmare fuel!");
+VALUES ("hello", 100, 1),
+       ("world", 200, 2),
+       ("green", 134, 3),
+       ("blue", 343, 4),
+       ("red", 929, 5);
+
+INSERT INTO employees(id, first_name, last_name, roles_id, manager_id)
+VALUES
+(1, 'frank', 'stevenson', '1', '1'),
+(2, 'Kobe', 'Bryant', '2', '2'),
+(3, 'john', 'trill', '3', '3'),
+(4, 'will', 'smith', '4', '4'),
+(5,'craig', 'robinson', '5', '5'),
+(6, 'jay', 'williams','1', '1'),
+(7, 'greg', 'seymour','2', '2');
